5434cad1bf624f392456f1d7b6772dfee6126440
Moshe Elhadad <44942115+moshelh@users.noreply.github.com>
2020-06-08 19:23:38 +0300

    first commit

---
+++ RPG project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform traget;
+
+    public Vector3 offset;
+    public float zoomSpeed =4f;
+    public float minZoom = 5f;
+    public float maxZoom = 15f;
+
+    public float pitch = 2f;
+
+    public float yawSpeed = 100f;
+
+    private float yawInput = 0f;
+    private float currentZoom = 10f;
+
+    public void Update()
+    {
+        currentZoom-=Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+
+        yawInput-=Input.GetAxis("Horizontal") * yawSpeed *Time.deltaTime;
+        
+    }
+
+    private void LateUpdate()
+    {
+        
+        transform.position = traget.position - offset * currentZoom;
+        transform.LookAt(transform.position + (Vector3.back+Vector3.down) * pitch);
+
+        transform.RotateAround(traget.position, Vector3.up, yawInput);
+
+    }
+}
---
+++ RPG project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    public Interactable focus;
+
+    public LayerMask mask;
+
+    Camera cam;
+
+    PlayerMotor motor;
+
+    void Start()
+    {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit,100,mask))
+            {
+                motor.MoveToPoint(hit.point);
+
+                RemoveFocus();
+            }
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+              Interactable interactable = hit.collider.GetComponent<Interactable>();
+              
+              if(interactable != null)
+                {
+                    SetFoucs(interactable);
+                }
+            }
+        }
+    }
+
+    private void RemoveFocus()
+    {
+        if(focus != null)
+            focus.OnDefocused();
+
+        focus = null;
+        motor.StopFollowingTarget();
+    }
+
+    private void SetFoucs(Interactable newInteractable)
+    {
+        if(newInteractable != focus)
+        {
+            if(focus !=null)
+                focus.OnDefocused();
+
+            focus = newInteractable;
+            motor.FollowTarget(newInteractable);
+
+        }
+        newInteractable.OnFoucused(transform);
+    }
+}
